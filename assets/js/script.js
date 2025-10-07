2f0666b4811b61f7eb9cb1f9a705cb035e88a878
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-10-07 17:41:56 +0200

    Fix Fizz Buzz and FizzBuzz message

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,15 +21,15 @@ for (let i = 1; i <= 100; i++) {
 
     //Stamp FizzBuzz for multiples of 3 and 5
     if (i % 3 === 0 && i % 5 === 0){
-        console.log(i, 'FizzBuzz');
+        console.log('FizzBuzz');
         
     } else if (i % 5 === 0) {
         //Stamp Buzz for multiples of 5
-        console.log(i, 'Buzz');
+        console.log('Buzz');
         
     }else if (i % 3 === 0) {
         //Stamp Fizz for multiples of 3
-        console.log(i, 'Fizz');
+        console.log('Fizz');
     } else {
         console.log(i);
     }
